2815749a9b35904a116ba1064dcb815d79ca3e40
Otávia Bedeschi <bedeschi.otavia@gmail.com>
2025-01-22 21:10:40 +0000

    Eslint config

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -10,13 +10,23 @@ interface Transaction {
   category: string;
   createdAt: string;
 }
+
+interface CreateTransactionInput {
+  description: string;
+  price: number;
+  category: string;
+  type: 'income' | 'outcome';
+}
+
 interface TransactionContextType {
   transactions: Transaction[];
   fetchTransactions: (query?: string) => Promise<void>;
+  createTransaction: (data: CreateTransactionInput) => Promise<void>;
 }
 interface TransactionsProviderProps {
-  children: ReactNode
+  children: ReactNode;
 }
+
 
 
 export const TransactionsContext = createContext({} as TransactionContextType);
@@ -28,10 +38,27 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
   async function fetchTransactions(query?: string) {
     const response = await api.get('transactions', {
       params:{
-        q:query
+        _sort: 'createdAt',
+        _order: 'desc',
+        q:query,
       }
     })
     setTransactions(response.data);
+  }
+
+  async function createTransaction(data: CreateTransactionInput){
+
+    const { description, price, category, type } = data;
+
+    const response = await api.post('transactions', {
+      description,
+      price,
+      category,
+      type,
+      createdAt: new Date(),
+    })
+
+    setTransactions(state => [response.data, ...state])
   }
 
   useEffect(() => {
@@ -42,6 +69,7 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
     <TransactionsContext.Provider value={{ 
       transactions,
       fetchTransactions,
+      createTransaction,
     }}>
       {children}
     </TransactionsContext.Provider>
