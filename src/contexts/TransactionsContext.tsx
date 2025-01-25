a3e93e750e41a6d2d9f4d1e014149d73b72077d1
Otávia Bedeschi <bedeschi.otavia@gmail.com>
2025-01-25 14:58:56 +0000

    Performance improvement contex-selector useCallback memo useMemo

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react-refresh/only-export-components */
-import { createContext, ReactNode, useEffect, useState } from 'react';
+import { ReactNode, useCallback, useEffect, useState } from 'react';
+import { createContext } from 'use-context-selector';
 import { api } from '../lib/axios';
 
 interface Transaction {
@@ -35,31 +36,35 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
 
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
-  async function fetchTransactions(query?: string) {
-    const response = await api.get('transactions', {
-      params:{
-        _sort: 'createdAt',
-        _order: 'desc',
-        q:query,
-      }
-    })
-    setTransactions(response.data);
-  }
+  const fetchTransactions = useCallback(async(query?: string) => {
+      const response = await api.get('transactions', {
+        params:{
+          _sort: 'createdAt',
+          _order: 'desc',
+          q:query,
+        }
+      })
+      setTransactions(response.data);
+    }, 
+    [],
+  )
 
-  async function createTransaction(data: CreateTransactionInput){
+  const createTransaction = useCallback(async (data: CreateTransactionInput) => {
 
-    const { description, price, category, type } = data;
+      const { description, price, category, type } = data;
 
-    const response = await api.post('transactions', {
-      description,
-      price,
-      category,
-      type,
-      createdAt: new Date(),
-    })
+      const response = await api.post('transactions', {
+        description,
+        price,
+        category,
+        type,
+        createdAt: new Date(),
+      })
 
-    setTransactions(state => [response.data, ...state])
-  }
+      setTransactions(state => [response.data, ...state])
+    }, 
+    [],
+  )
 
   useEffect(() => {
     fetchTransactions();
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,16 +1,18 @@
-import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { SearchForm } from "../../components/SearchForm";
 
 import { Summary } from "../../components/Summary";
 
+import { useContextSelector } from "use-context-selector";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
 import { dateFormatter, priceFormatter } from "../../utils/formatter";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
 
-  const { transactions } = useContext(TransactionsContext)
+  const transactions = useContextSelector(TransactionsContext,(context) => {
+    return context.transactions
+  })
   return (
     <>
       <Header />
